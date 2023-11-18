677c488c7a08f7300192a097dbb67a482a8d8eeb
kafishh <kengarookangaroo0716@gmail.com>
2023-11-18 09:38:56 +0800

    Initial Commit

---
+++ CKJY Production Studios SAP/CKJY_Production_Studios_SAPApp.swift
@@ -0,0 +1,17 @@
+//
+//  CKJY_Production_Studios_SAPApp.swift
+//  CKJY Production Studios SAP
+//
+//  Created by RGS on 18/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CKJY_Production_Studios_SAPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CKJY Production Studios SAP/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CKJY Production Studios SAP
+//
+//  Created by RGS on 18/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
